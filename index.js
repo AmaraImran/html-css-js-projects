1e5d58bf627394dcec681c1d60f28226917f6598
AmaraImran <khanamara265@gmail.com>
2025-01-28 18:47:12 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function clicked(){
+   let pop=document.getElementById('pop')
+pop.classList.add("openpopup")
+}
+function closepopup(){
+    let pop=document.getElementById('pop')
+    pop.classList.remove("openpopup")
+}
